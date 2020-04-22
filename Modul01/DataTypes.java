35c4bb6fe005cdbcf5245ef577210f05e8901068
muhamadnoval <mnoval292@gmail.com>
2020-04-22 08:35:34 +0700

    Output dengan sout

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -2,13 +2,20 @@ public class DataTypes{
 	public static void main(String[] args){
 	
 		String namaDepan = "Muhamad";
-		String namaTengah = "Noval";
-		String namaBelakang = "Kurniawan";
+		String namaBelakang = "Noval";
 		int usia = 20;
 		int targetTahunKuliah = 4;
 		double ipk = 3.89764512;
 		char nilaiAbjad = 'A';
 		boolean tampan = true;
-		
+			
+		System.out.println("Nama depan : " + namaDepan);
+		System.out.println("Nama tengah : " + namaBelakang);
+		System.out.println("Usia : " + usia);
+		System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+		System.out.println("IPK : " + ipk);
+		System.out.println("Nilai PBO : " + nilaiAbjad);
+		System.out.println("Tampan : " + tampan);
 	}
+	
 }
