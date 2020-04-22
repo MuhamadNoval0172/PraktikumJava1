9d85625a345fe942d2e2ae276a8738e08d7c5895
muhamadnoval <mnoval292@gmail.com>
2020-04-22 07:49:29 +0700

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+	public static void main(String[] args){
+	
+		String namaDepan = "Muhamad";
+		String namaTengah = "Noval";
+		String namaBelakang = "Kurniawan";
+		int usia = 20;
+		int targetTahunKuliah = 4;
+		double ipk = 3.89764512;
+		char nilaiAbjad = 'A';
+		boolean tampan = true;
+		
+	}
+}
